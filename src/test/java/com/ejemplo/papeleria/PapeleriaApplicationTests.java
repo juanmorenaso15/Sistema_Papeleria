f69001171d450a66e613f3561bbfc02144d01f1d
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-09 23:05:25 -0500

    subo Carpeta completa

---
+++ src/test/java/com/ejemplo/papeleria/PapeleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.papeleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapeleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
